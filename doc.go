5c21588ce05aba874fb3fd5aee15c18b91fbeced
Brian Sorahan <bsorahan@gmail.com>
2015-10-18 08:46:15 -0500

    initial import from sc

---
+++ doc.go
@@ -0,0 +1,8 @@
+// pattern is a go package that provides a way to create
+// musical scores similar to the SuperCollider patterns API
+// http://doc.sccode.org/Tutorials/Streams-Patterns-Events1.html.
+package pattern
+
+const (
+	Inf = -1
+)
