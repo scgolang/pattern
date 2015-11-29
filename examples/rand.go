db2a3e9bf276d5dd9b9b03dc168200ba8581e459
Brian Sorahan <bsorahan@leaf.ag>
2015-11-28 23:04:07 -0600

    use Select in rand example

--- examples/rand.go
+++ examples/rand.go
@@ -10,15 +10,18 @@ import (
 )
 
 const (
-	OctaveMin = 1
-	OctaveMax = 7
+	DefName   = "pattern_examples_rand"
+	OctaveMin = 3
+	OctaveMax = 5
 )
 
 var (
-	DefSine = sc.NewSynthdef("testSine", func(p sc.Params) sc.Ugen {
+	Def = sc.NewSynthdef(DefName, func(p sc.Params) sc.Ugen {
 		bus := sc.C(0)
 		gate, freq := p.Add("gate", 1), p.Add("freq", 440)
 		gain, release := p.Add("gain", 1), p.Add("release", 0.2)
+		timbre := p.Add("timbre", 0)
+
 		envgen := sc.EnvGen{
 			Env:        sc.EnvPerc{Release: release},
 			Gate:       gate,
@@ -26,45 +29,14 @@ var (
 			Done:       sc.FreeEnclosing,
 		}.Rate(sc.KR)
 
-		sig := sc.SinOsc{Freq: freq}.Rate(sc.AR).Mul(envgen)
+		sine := sc.SinOsc{Freq: freq}.Rate(sc.AR).Mul(envgen)
+		blip := sc.Blip{Freq: freq}.Rate(sc.AR).Mul(envgen)
+		saw := sc.Saw{Freq: freq}.Rate(sc.AR).Mul(envgen)
 
-		// make it stereo
-		sig = sc.Multi(sig, sig)
-
-		return sc.Out{bus, sig}.Rate(sc.AR)
-	})
-
-	DefBlip = sc.NewSynthdef("testBlip", func(p sc.Params) sc.Ugen {
-		bus := sc.C(0)
-		gate, freq := p.Add("gate", 1), p.Add("freq", 440)
-		gain, release := p.Add("gain", 1), p.Add("release", 0.2)
-		envgen := sc.EnvGen{
-			Env:        sc.EnvPerc{Release: release},
-			Gate:       gate,
-			LevelScale: gain,
-			Done:       sc.FreeEnclosing,
-		}.Rate(sc.KR)
-
-		sig := sc.Blip{Freq: freq}.Rate(sc.AR).Mul(envgen)
-
-		// make it stereo
-		sig = sc.Multi(sig, sig)
-
-		return sc.Out{bus, sig}.Rate(sc.AR)
-	})
-
-	DefSaw = sc.NewSynthdef("testSaw", func(p sc.Params) sc.Ugen {
-		bus := sc.C(0)
-		gate, freq := p.Add("gate", 1), p.Add("freq", 440)
-		gain, release := p.Add("gain", 1), p.Add("release", 0.2)
-		envgen := sc.EnvGen{
-			Env:        sc.EnvPerc{Release: release},
-			Gate:       gate,
-			LevelScale: gain,
-			Done:       sc.FreeEnclosing,
-		}.Rate(sc.KR)
-
-		sig := sc.Saw{Freq: freq}.Rate(sc.AR).Mul(envgen)
+		sig := sc.Select{
+			Which:  timbre,
+			Inputs: []sc.Input{sine, blip, saw},
+		}.Rate(sc.AR)
 
 		// make it stereo
 		sig = sc.Multi(sig, sig)
@@ -81,20 +53,17 @@ var (
 		"timbre":  pattern.Rand(pattern.Inf, []float32{0, 1, 2}),
 	}
 
-	Defs = []string{"testSine", "testBlip", "testSaw"}
-
 	Events = pattern.Pbind{
 		Instruments: func() string {
-			return Defs[rand.Intn(len(Defs))]
+			return DefName
 		},
 		Controls: Controls,
 	}
 
 	durations = []time.Duration{
-		62 * time.Millisecond,
-		125 * time.Millisecond,
-		187 * time.Millisecond,
-		250 * time.Millisecond,
+		// 64 * time.Millisecond,
+		128 * time.Millisecond,
+		256 * time.Millisecond,
 	}
 
 	Durations = func() time.Duration {
@@ -108,13 +77,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := client.SendDef(DefSine); err != nil {
-		log.Fatal(err)
-	}
-	if err := client.SendDef(DefBlip); err != nil {
-		log.Fatal(err)
-	}
-	if err := client.SendDef(DefSaw); err != nil {
+	if err := client.SendDef(Def); err != nil {
 		log.Fatal(err)
 	}
 
@@ -141,7 +104,7 @@ func randomNotes() pattern.CtrlFunc {
 	i, scale := 0, scales[rand.Intn(len(scales))]
 
 	return func() float32 {
-		if i%32 == 0 {
+		if i%128 == 0 {
 			scale = scales[rand.Intn(len(scales))]
 			i = 0
 		}
